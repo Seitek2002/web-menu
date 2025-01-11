09047ba0bedd9ee1e46ad656e7044ae5f2413da3
Seitek2002 <seitekbegaliev@gmail.com>
2025-01-11 22:16:38 +0600

    reworking

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
-
+import logo from '../../assets/icons/Header/logo.svg';
+import table from '../../assets/icons/Header/table.svg';
 import './style.scss';
 
 const Header: FC = () => {
@@ -7,8 +8,15 @@ const Header: FC = () => {
     <header className='header'>
       <div className='container'>
         <div className='header__content'>
-          <div className='header__num'>№12</div>
-          <h2 className='font-benzin'>Лого</h2>
+          <div className="header__logo">
+            <img src={logo} alt="logo" />
+            <div className="header__info">
+              <p className="header__name">Жираф</p> 
+              <div className="header__inner">
+                <img src={table} alt="icon" /> <p className="header__table">Стол №12</p>
+              </div>
+            </div>
+          </div>
           <select defaultValue={'ru'}>
             <option value='ru'>RU</option>
             <option value='kg'>KG</option>
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -12,18 +12,28 @@ const Hero: FC = () => {
       centeredSlides={true}
     >
       <SwiperSlide>
+        <div className='swiper__item another'>  
+              <div className="swiper__top">
+                <h4 className="swiper__name">Adam Ventures</h4> <img src="/src/assets/icons/hero/arrow.svg" alt="arrow" />
+              </div> 
+              <p className="swiper__text">Заказывай еду и не трать время! <br/> (Какой нибудь текст про платформу)</p>
+            
+            <img className='swiper__burger' src="/src/assets/icons/hero/burger.svg" alt="burger" />
+          </div>
+      </SwiperSlide>
+      <SwiperSlide>
         <div className='swiper__item'>
-          <img src='/src/assets/images/Slider/img-1.png' alt='' />
+          <img className='swiper__bg' src='/src/assets/images/Slider/img-1.png' alt='' />
         </div>
       </SwiperSlide>
       <SwiperSlide>
         <div className='swiper__item'>
-          <img src='/src/assets/images/Slider/img-2.jpg' alt='' />
+          <img className='swiper__bg' src='/src/assets/images/Slider/img-2.jpg' alt='' />
         </div>
       </SwiperSlide>
       <SwiperSlide>
         <div className='swiper__item'>
-          <img src='/src/assets/images/Slider/img-1.png' alt='' />
+          <img className='swiper__bg' src='/src/assets/images/Slider/img-1.png' alt='' />
         </div>
       </SwiperSlide>
     </Swiper>
