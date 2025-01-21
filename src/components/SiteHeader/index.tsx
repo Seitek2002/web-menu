a154f42e8b8ac65e8b76e9cef2912dc35a9070b7
Seitek2002 <seitekbegaliev@gmail.com>
2025-01-21 19:10:17 +0600

    refact

--- src/components/SiteHeader/index.tsx
+++ src/components/SiteHeader/index.tsx
@@ -1,53 +1,79 @@
-import { FC, useState } from "react";
+import { FC, useState } from 'react';
 
-import "./style.scss";
+import './style.scss';
 
-import logo from "../../assets/icons/siteHeader/logo.svg";
-import arrow from "../../assets/icons/header/black-arrow.svg";
-import user from "../../assets/icons/SiteHeader/user.svg"
+import logo from '../../assets/icons/siteHeader/logo.svg';
+import arrow from '../../assets/icons/header/black-arrow.svg';
+import user from '../../assets/icons/SiteHeader/user.svg';
 
 const siteHeader: FC = () => {
-    const [isLanguageOpen, setIsLanguageOpen] = useState(false);
-    const [active, setActive] = useState("RU");
+  const [isLanguageOpen, setIsLanguageOpen] = useState(false);
+  const [active, setActive] = useState('RU');
 
-    const toggleLanguageMenu = () => {
-        setIsLanguageOpen((prev) => !prev);
-    };
+  const toggleLanguageMenu = () => {
+    setIsLanguageOpen((prev) => !prev);
+  };
 
-    const selectLanguage = (language: string) => {
-        setActive(language); // Устанавливаем активный язык
-        setIsLanguageOpen(false); // Закрываем меню после выбора
-    };
+  const selectLanguage = (language: string) => {
+    setActive(language);
+    setIsLanguageOpen(false);
+  };
 
-  const list = ["RU","KG","ENG"]
+  const list = ['RU', 'KG', 'ENG'];
 
-    return (
-        <header className="siteHeader">
-            <div className="container">
-                <div className="siteHeader__content">
-                    <div className="siteHeader__logo">
-                        <img src={logo} alt="logo" />
-                        <div className="siteHeader__info">
-                            <p className="siteHeader__name">iMenu.kg</p>
-                        </div>
-                    </div>
-                    <div className="siteHeader__block">
-                        <div className="siteHeader__user">
-                            <img src={user} alt="user" />
-                        </div>
-                        <div className={isLanguageOpen ? "siteHeader__language active" : "siteHeader__language"}>
-                            <div className={`siteHeader__language-selected ${ isLanguageOpen ? "active" : "" }`} onClick={toggleLanguageMenu} > {active} <img src={arrow} alt="arrow" /> </div>
-                            <div className={`siteHeader__wrapper ${isLanguageOpen ? "active" : ""}`}>
-                                {list.filter((item) => item != active).map((item) =>  (
-                            <div className="siteHeader__item" onClick={() => selectLanguage(item)} key={item}>{item}</div>
-                             ))}
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <header className='siteHeader'>
+      <div className='container'>
+        <div className='siteHeader__content'>
+          <div className='siteHeader__logo'>
+            <img src={logo} alt='logo' />
+            <div className='siteHeader__info'>
+              <p className='siteHeader__name'>iMenu.kg</p>
             </div>
-        </header>
-    );
+          </div>
+          <div className='siteHeader__block'>
+            <div className='siteHeader__user'>
+              <img src={user} alt='user' />
+            </div>
+            <div
+              className={
+                isLanguageOpen
+                  ? 'siteHeader__language active'
+                  : 'siteHeader__language'
+              }
+            >
+              <div
+                className={`siteHeader__language-selected ${
+                  isLanguageOpen ? 'active' : ''
+                }`}
+                onClick={toggleLanguageMenu}
+              >
+                {' '}
+                {active} <img src={arrow} alt='arrow' />{' '}
+              </div>
+              <div
+                className={`siteHeader__wrapper ${
+                  isLanguageOpen ? 'active' : ''
+                }`}
+              >
+                {list
+                  .filter((item) => item != active)
+                  .map((item) => (
+                    <div
+                      className='siteHeader__item'
+                      onClick={() => selectLanguage(item)}
+                      key={item}
+                    >
+                      {item}
+                    </div>
+                  ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default siteHeader;
