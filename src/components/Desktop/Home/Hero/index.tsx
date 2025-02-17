95ee2f63a5df09980073d49f6e392061efd31393
laby <dipt688@gmail.com>
2025-02-17 22:34:40 +0600

    shift + alt + f

--- src/components/Desktop/Home/Hero/index.tsx
+++ src/components/Desktop/Home/Hero/index.tsx
@@ -12,70 +12,70 @@ import icon from "../../../../assets/icons/Hero/icon.svg";
 import "./style.scss";
 
 const Hero: FC = () => {
-    const { t } = useTranslation();
-    return (
-        <>
-            <Swiper
-                className="desktop swiper"
-                spaceBetween={8}
-                slidesPerView={1.2}
-                centeredSlides={true}
-                pagination={{
-                    el: ".desktop.custom-pagination",
-                    clickable: true,
-                    renderBullet: (_, className) => {
-                        return `<span class="${className} desktop pagination-bullet"></span>`;
-                    },
-                }}
-                modules={[Pagination]}
-            >
-                <SwiperSlide>
-                    <div className="desktop swiper__item">
-                        <OrderStatusCard
-                            title={t("statusCard.accepted")}
-                            message={t("statusCard.review")}
-                            status="accepted"
-                        />
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="desktop swiper__item">
-                        <OrderStatusCard
-                            title={t("message.accepted")}
-                            message={t("message.review")}
-                            status="pending"
-                        />
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="desktop swiper__item another">
-                        <div className="desktop swiper__top">
-                            <h4 className="desktop swiper__name text-white">iMenu.kg</h4>
-                            <img src={arrow} alt="arrow" />
-                        </div>
-                        <p className="desktop swiper__text">-{t("swiperText")}</p>
-                        <img className="desktop swiper__burger" src={icon} alt="burger" />
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="desktop swiper__item">
-                        <img className="desktop swiper__bg" src={one} alt="Slide 1" />
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="desktop swiper__item">
-                        <img className="desktop swiper__bg" src={two} alt="Slide 2" />
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="desktop swiper__item">
-                        <img className="desktop swiper__bg" src={one} alt="Slide 3" />
-                    </div>
-                </SwiperSlide>
-            </Swiper>
-            <div className="desktop custom-pagination"></div>
-        </>
-    );
+  const { t } = useTranslation();
+  return (
+    <>
+      <Swiper
+        className="desktop swiper"
+        spaceBetween={8}
+        slidesPerView={1.2}
+        centeredSlides={true}
+        pagination={{
+          el: ".desktop.custom-pagination",
+          clickable: true,
+          renderBullet: (_, className) => {
+            return `<span class="${className} desktop pagination-bullet"></span>`;
+          },
+        }}
+        modules={[Pagination]}
+      >
+        <SwiperSlide>
+          <div className="desktop swiper__item">
+            <OrderStatusCard
+              title={t("statusCard.accepted")}
+              message={t("statusCard.review")}
+              status="accepted"
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="desktop swiper__item">
+            <OrderStatusCard
+              title={t("message.accepted")}
+              message={t("message.review")}
+              status="pending"
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="desktop swiper__item another">
+            <div className="desktop swiper__top">
+              <h4 className="desktop swiper__name text-white">iMenu.kg</h4>
+              <img src={arrow} alt="arrow" />
+            </div>
+            <p className="desktop swiper__text">-{t("swiperText")}</p>
+            <img className="desktop swiper__burger" src={icon} alt="burger" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="desktop swiper__item">
+            <img className="desktop swiper__bg" src={one} alt="Slide 1" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="desktop swiper__item">
+            <img className="desktop swiper__bg" src={two} alt="Slide 2" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="desktop swiper__item">
+            <img className="desktop swiper__bg" src={one} alt="Slide 3" />
+          </div>
+        </SwiperSlide>
+      </Swiper>
+      <div className="desktop custom-pagination"></div>
+    </>
+  );
 };
 
 export default Hero;
--- src/components/Mobile/Home/Hero/index.tsx
+++ src/components/Mobile/Home/Hero/index.tsx
@@ -11,62 +11,62 @@ import icon from "../../../../assets/icons/Hero/icon.svg";
 import "./style.scss";
 
 const Hero: FC = () => {
-    const { t } = useTranslation();
-    return (
-        <Swiper
-            className="mobile swiper"
-            spaceBetween={8}
-            slidesPerView={1.2}
-            centeredSlides={true}
-        >
-            <SwiperSlide>
-                <div className="mobile swiper__item">
-                    <OrderStatusCard
-                        title={t("statusCard.accepted")}
-                        message={t("statusCard.review")}
-                        status="accepted"
-                    />
-                </div>
-            </SwiperSlide>
-            <SwiperSlide>
-                <div className="mobile swiper__item">
-                    <OrderStatusCard
-                        title={t("message.accepted")}
-                        message={t("message.review")}
-                        status="pending"
-                    />
-                </div>
-            </SwiperSlide>
-            <SwiperSlide>
-                <div className="mobile swiper__item another">
-                    <div className="mobile swiper__top">
-                        <h4 className="mobile swiper__name text-[#fff]">iMenu.kg</h4>
-                        <img src={arrow} alt="arrow" />
-                    </div>
-                    <p className="mobile swiper__text text-[#DFC0DD]">
-                        -{t("swiperText")}
-                    </p>
+  const { t } = useTranslation();
+  return (
+    <Swiper
+      className="mobile swiper"
+      spaceBetween={8}
+      slidesPerView={1.2}
+      centeredSlides={true}
+    >
+      <SwiperSlide>
+        <div className="mobile swiper__item">
+          <OrderStatusCard
+            title={t("statusCard.accepted")}
+            message={t("statusCard.review")}
+            status="accepted"
+          />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="mobile swiper__item">
+          <OrderStatusCard
+            title={t("message.accepted")}
+            message={t("message.review")}
+            status="pending"
+          />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="mobile swiper__item another">
+          <div className="mobile swiper__top">
+            <h4 className="mobile swiper__name text-[#fff]">iMenu.kg</h4>
+            <img src={arrow} alt="arrow" />
+          </div>
+          <p className="mobile swiper__text text-[#DFC0DD]">
+            -{t("swiperText")}
+          </p>
 
-                    <img className="mobile swiper__burger" src={icon} alt="burger" />
-                </div>
-            </SwiperSlide>
-            <SwiperSlide>
-                <div className="mobile swiper__item">
-                    <img className="mobile swiper__bg" src={one} alt="" />
-                </div>
-            </SwiperSlide>
-            <SwiperSlide>
-                <div className="mobile swiper__item">
-                    <img className="mobile swiper__bg" src={two} alt="" />
-                </div>
-            </SwiperSlide>
-            <SwiperSlide>
-                <div className="mobile swiper__item">
-                    <img className="mobile swiper__bg" src={one} alt="" />
-                </div>
-            </SwiperSlide>
-        </Swiper>
-    );
+          <img className="mobile swiper__burger" src={icon} alt="burger" />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="mobile swiper__item">
+          <img className="mobile swiper__bg" src={one} alt="" />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="mobile swiper__item">
+          <img className="mobile swiper__bg" src={two} alt="" />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="mobile swiper__item">
+          <img className="mobile swiper__bg" src={one} alt="" />
+        </div>
+      </SwiperSlide>
+    </Swiper>
+  );
 };
 
 export default Hero;
