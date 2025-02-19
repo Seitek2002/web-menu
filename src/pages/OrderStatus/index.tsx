366ac6e1485e121e01c5da4bb294ede0b2cc430f
imka2006 <31unknown12@gmail.com>
2025-02-20 01:35:28 +0600

    created components for order status, code cleaning and optimization

--- src/pages/OrderStatus/index.tsx
+++ src/pages/OrderStatus/index.tsx
@@ -1,185 +1,64 @@
 import { FC } from "react";
 
-import back from "../../assets/icons/OrderStatus/back.svg";
-import share from "../../assets/icons/OrderStatus/share.svg";
-import table from "../../assets/icons/OrderStatus/table.svg";
+import Head from "src/components/OrderStatus/Head";
+import ListItem from "src/components/OrderStatus/List/ListItem";
+import ListHead from "src/components/OrderStatus/List/ListHead";
+import Status from "src/components/OrderStatus/Status";
+
 import burger from "../../assets/images/Catalog/item-2.webp";
-import cake from "../../assets/images/Catalog/item-5.webp";
-import panCake from "../../assets/images/Catalog/item-4.webp";
-import doing from "../../assets/images/OrderStatus/doing.png";
-import wait from "../../assets/images/OrderStatus/wait.png";
-import cencel from "../../assets/images/OrderStatus/cencel.png";
-import doingIcon from "../../assets/icons/OrderStatus/doing.svg";
-import waitIcont from "../../assets/icons/OrderStatus/wait.svg";
+import shawarma from "../../assets/images/Catalog/item-1.webp";
+import salad from "../../assets/images/OrderStatus/salad.png";
+import cake from "../../assets/images/OrderStatus/cake.png";
+import pancakes from "../../assets/images/OrderStatus/pancakes.png";
 
 import "./style.scss";
 
+interface Order {
+    id: number;
+    img: string;
+    name: string;
+    price: number;
+    weight: number;
+    quantity: number;
+}
+
+const myOrders: Order[] = [
+    { id: 0, img: salad, name: "Салат с креветками", price: 200, weight: 200, quantity: 2 },
+    { id: 1, img: shawarma, name: "Двойная запеченная шаурма", price: 400, weight: 500, quantity: 2 },
+];
+
+const allOrders: Order[] = [
+    { id: 2, img: burger, name: "Бургер стандартный", price: 600, weight: 300, quantity: 2 },
+    { id: 3, img: cake, name: "Шоколадный торт", price: 480, weight: 120, quantity: 2 },
+    { id: 4, img: pancakes, name: "Японские панкейки с ягодами и медовым сиропом", price: 230, weight: 350, quantity: 1 },
+    ...myOrders, 
+];
+
 const OrderStatus: FC = () => {
     return (
-        <section className="os">
+        <section className="order__status">
             <div className="container">
-                <div className="order__status-head">
-                    <div className="order__status-img-wrapper bg-white">
-                        <img src={back} alt="back" />
-                    </div>
-                    <h2 className="order__status-title text-[#090A0B]">Мой заказ</h2>
-                    <div className="order__status-img-wrapper bg-white">
-                        <img src={share} alt="share" />
-                    </div>
-                </div>
-
+                <Head />
                 <div className="order__status-wrapper">
                     <div className="order__status-list bg-white shadow rounded">
-                        <div className="order__status-list-head">
-                            <h4 className="order__status-list-title text-[#090A0B]">Мои заказы</h4>
-                              <div className="order__status-list-order-quantity text-[#727272]">
-                                4 заказов
-                            </div>
-                        </div>
+                        {/* Мои заказы */}
+                        <ListHead table={0} quantity={myOrders.length} />
                         <div className="order__status-list-content divide-y-2 divide-gray-300">
-                            <div className="order__status-list-item">
-                                <div className="order__status-list-img-wrapper">
-                                    <img src={burger} alt="img" />
-                                </div>
-                                <div className="order__status-list-info-wrapper">
-                                    <div className="order__status-list-info">
-                                        <h5 className="order__status-list-name text-[#090A0B]">
-                                            Бургер стандартный
-                                        </h5>
-                                        <span className="order__status-list-weight text-[#727272]">
-                                            300 г
-                                        </span>
-                                    </div>
-                                    <div className="order__status-list-info">
-                                        <span className="order__status-list-price text-[#875AFF]">
-                                            500 c
-                                        </span>
-                                        <span className="order__status-list-quantity text-[#727272]">
-                                            2 шт
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="order__status-list-item">
-                                <div className="order__status-list-img-wrapper">
-                                    <img src={cake} alt="img" />
-                                </div>
-                                <div className="order__status-list-info-wrapper">
-                                    <div className="order__status-list-info">
-                                        <h5 className="order__status-list-name text-[#090A0B]">
-                                        Шоколадный торт
-                                        </h5>
-                                        <span className="order__status-list-weight text-[#727272]">
-                                        120г
-                                        </span>
-                                    </div>
-                                    <div className="order__status-list-info">
-                                        <span className="order__status-list-price text-[#875AFF]">
-                                            480 с
-                                        </span>
-                                        <span className="order__status-list-quantity text-[#727272]">
-                                            2 шт
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
+                            {myOrders.map((item) => (
+                                <ListItem key={item.id} {...item} />
+                            ))}
                         </div>
-                        <div className="order__status-list-head another">
-                            <h4 className="order__status-list-title text-[#090A0B]">Заказы стола №12</h4>
-                            <div className="order__status-list-order-quantity text-[#727272]">
-                                5 заказов
-                            </div>
-                        </div>
+
+                        {/* Заказы стола №12 */}
+                        <ListHead table={12} quantity={allOrders.length} />
                         <div className="order__status-list-content divide-y-2 divide-gray-300">
-                            <div className="order__status-list-item">
-                                <div className="order__status-list-img-wrapper">
-                                    <img src={burger} alt="img" />
-                                </div>
-                                <div className="order__status-list-info-wrapper">
-                                    <div className="order__status-list-info">
-                                        <h5 className="order__status-list-name text-[#090A0B]">
-                                            Бургер стандартный
-                                        </h5>
-                                        <span className="order__status-list-weight text-[#727272]">
-                                            300 г
-                                        </span>
-                                    </div>
-                                    <div className="order__status-list-info">
-                                        <span className="order__status-list-price text-[#875AFF]">
-                                            500 c
-                                        </span>
-                                        <span className="order__status-list-quantity text-[#727272]">
-                                            2 шт
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="order__status-list-item">
-                                <div className="order__status-list-img-wrapper">
-                                    <img src={cake} alt="img" />
-                                </div>
-                                <div className="order__status-list-info-wrapper">
-                                    <div className="order__status-list-info">
-                                        <h5 className="order__status-list-name text-[#090A0B]">
-                                        Шоколадный торт
-                                        </h5>
-                                        <span className="order__status-list-weight text-[#727272]">
-                                        120г
-                                        </span>
-                                    </div>
-                                    <div className="order__status-list-info">
-                                        <span className="order__status-list-price text-[#875AFF]">
-                                            480 с
-                                        </span>
-                                        <span className="order__status-list-quantity text-[#727272]">
-                                            2 шт
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="order__status-list-item">
-                                <div className="order__status-list-img-wrapper">
-                                    <img src={panCake} alt="img" />
-                                </div>
-                                <div className="order__status-list-info-wrapper">
-                                    <div className="order__status-list-info">
-                                        <h5 className="order__status-list-name text-[#090A0B]">
-                                            Японские панкейки с ягодами и медовым сиропом
-                                        </h5>
-                                        <span className="order__status-list-weight text-[#727272]">
-                                            350 г
-                                        </span>
-                                    </div>
-                                    <div className="order__status-list-info">
-                                        <span className="order__status-list-price text-[#875AFF]">
-                                            230 с
-                                        </span>
-                                        <span className="order__status-list-quantity text-[#727272]">
-                                            1 шт
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
+                            {allOrders.map((item) => (
+                                <ListItem key={item.id} {...item} />
+                            ))}
                         </div>
                     </div>
-
-                    <div className="order__status-status bg-white shadow rounded self-start">
-                      <h4 className="order__status-status-title text-[#875AFF]"> <img src={table} alt="table" />Стол №12</h4>
-
-                        <img src={doing} alt="doing" className="order__status-status-img" />
-                        <div className="order__status-status-share bg-white"><img src={share} alt="doing"  /></div>
-
-
-                        {/* <img src={doingIcon} alt="doing" /> */}
-                        <img src={waitIcont} alt="wait" />
-                        {/* <img src={wait} alt="wait" />
-                        <img src={cencel} alt="cencel" /> */}
-
-                          <h4 className="order__status-status-subtitle text-[#FF8400]">Спасибо заказ в ожидании</h4>
-                          <p className="order__status-status-description text-[#ADADAD]">В ближайшие 5-10 минут администратор свяжется с Вами и уточнит детали</p>
-
-                        <button className="order__status-status-btn text-[#fff]">Заказать еще</button>
-                    </div>
+                    {/* Статус заказа */}
+                    <Status />
                 </div>
             </div>
         </section>
