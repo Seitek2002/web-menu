73c1b73fab1f51980771767c4362b54128156e93
Seitek2002 <seitekbegaliev@gmail.com>
2025-02-20 14:42:42 +0600

    user-hidden

--- src/components/Desktop/Home/SiteHeader/index.tsx
+++ src/components/Desktop/Home/SiteHeader/index.tsx
@@ -59,9 +59,9 @@ const SiteHeader: FC = () => {
               <input type="text" placeholder={t("search")} />
             </div>
             <div className="siteHeader__block">
-              <div className="siteHeader__user bg-[#F9F9F9]">
+              {/* <div className="siteHeader__user bg-[#F9F9F9]">
                 <img src={user} alt="user" />
-              </div>
+              </div> */}
               <div className="siteHeader__language">
                 <div
                   className={`siteHeader__language-selected bg-[#F9F9F9] ${
