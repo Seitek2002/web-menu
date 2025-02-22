0bf7e44fada98a4932bbee962a9035829f3a09eb
laby <dipt688@gmail.com>
2025-02-23 00:19:00 +0600

    add effcet sticky and smooth scrolling

--- src/components/Cards/Catalog.tsx
+++ src/components/Cards/Catalog.tsx
@@ -1,14 +1,19 @@
-import { FC, useState } from 'react';
-import { addItem, removeItem } from '../../store/yourFeatureSlice';
-import { useAppDispatch } from '../../hooks/useAppDispatch';
-import ContentLoader from 'react-content-loader';
-import { IProductCatalog } from 'src/types/products.types';
+import { FC, useState } from "react";
+import { addItem, removeItem } from "../../store/yourFeatureSlice";
+import { useAppDispatch } from "../../hooks/useAppDispatch";
+import ContentLoader from "react-content-loader";
+import { IProductCatalog } from "src/types/products.types";
 
-import whitePlus from '../../assets/icons/cart/plus.svg';
-import whiteMinus from '../../assets/icons/cart/minus.svg';
+import whitePlus from "../../assets/icons/cart/plus.svg";
+import whiteMinus from "../../assets/icons/cart/minus.svg";
 
-import './style.scss'
-
+import "./style.scss";
+const ScrollClick = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
 interface IProps extends IProductCatalog {
   setIsShow: () => void;
   quantity: number;
@@ -63,44 +68,50 @@ const CatalogCard: FC<IProps> = ({
   };
 
   return (
-    <div className='cart-block bg-white'>
-      <div className='cart-img'>
+    <div className="cart-block bg-white">
+      <div className="cart-img">
         {!isLoaded && (
           <ContentLoader
             speed={1.5}
-            width='100%'
-            height='100%'
-            backgroundColor='#bebebe'
-            foregroundColor='#fff'
+            width="100%"
+            height="100%"
+            backgroundColor="#bebebe"
+            foregroundColor="#fff"
             style={{
-              padding: '4px',
+              padding: "4px",
             }}
           >
-            <rect className='skeleton-img' y='0' rx='12' ry='12' />
+            <rect className="skeleton-img" y="0" rx="12" ry="12" />
           </ContentLoader>
         )}
         <img
           src={productPhoto}
-          alt='img'
+          alt="img"
           onLoad={() => setIsLoaded(true)}
-          className={isLoaded ? '' : 'hidden'}
+          className={isLoaded ? "" : "hidden"}
           onClick={setIsShow}
         />
       </div>
-      <div className='cart-info'>
-        <span className='cart-price text-[#875AFF]'>{productPrice} с</span>
+      <div className="cart-info">
+        <span className="cart-price text-[#875AFF]">{productPrice} с</span>
         {/* <span className="cart-weight text-[#ADADAD]">•{category.categoryName}</span> */}
       </div>
-      <h4 className='cart-name'>{productName}</h4>
+      <h4 className="cart-name">{productName}</h4>
       {quantity === 0 ? (
-        <button className='cart-btn bg-[#F1F2F3] text-[#000]' onClick={handleClick}>
+        <button
+          className="cart-btn bg-[#F1F2F3] text-[#000]"
+          onClick={() => {
+            handleClick();
+            ScrollClick();
+          }}
+        >
           Добавить
         </button>
       ) : (
-        <div className='cart-btn active bg-[#875AFF]'>
-          <img onClick={handleUnClick} src={whiteMinus} alt='minus' />
-          <span className='cart-count text-[#fff]'>{quantity}</span>
-          <img onClick={handleClick} src={whitePlus} alt='plus' />
+        <div className="cart-btn active bg-[#875AFF]">
+          <img onClick={handleUnClick} src={whiteMinus} alt="minus" />
+          <span className="cart-count text-[#fff]">{quantity}</span>
+          <img onClick={handleClick} src={whitePlus} alt="plus" />
         </div>
       )}
     </div>
