ac2e00ca2099638af9b37f91d0c714d531b82493
Seitek2002 <seitekbegaliev@gmail.com>
2025-03-03 12:56:07 +0600

    new api add

---
+++ src/api/Orders.api.ts
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { IOrder } from 'src/types/orders.types';
+
+export const Orders = createApi({
+  reducerPath: 'ordersApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://imenu.kg/api/' }),
+  endpoints: (builder) => ({
+    getOrders: builder.query<IOrder[], { tableNum?: string; venueSlug?: string; spotSlug?: string }>({
+      query: ({ tableNum, venueSlug, spotSlug }) => {
+        const params = new URLSearchParams();
+        if (tableNum) params.append('tableNum', tableNum);
+        if (venueSlug) params.append('venueSlug', venueSlug);
+        if (spotSlug) params.append('spotSlug', spotSlug);
+
+        return `orders/?${params.toString()}`;
+      },
+    }),
+    postOrders: builder.mutation<void, IOrder>({
+      query: (newOrder) => ({
+        url: 'orders',
+        method: 'POST',
+        body: newOrder,
+      }),
+    }),
+  }),
+});
+
+export const { useGetOrdersQuery, usePostOrdersMutation } = Orders;
