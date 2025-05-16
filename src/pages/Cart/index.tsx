fc449eb11db55c14197f700b8ec4c93f464a4c20
Seitek2002 <seitekbegaliev@gmail.com>
2025-05-17 04:01:25 +0600

    changes

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -40,9 +40,11 @@ const Cart: React.FC = () => {
   const venueData = useAppSelector((state) => state.yourFeature.venue);
 
   const [activeIndex, setActiveIndex] = useState(0);
+
   const [phoneNumber, setPhoneNumber] = useState(`+${userData.phoneNumber}`);
   const [comment, setComment] = useState(userData.comment || '');
   const [address, setAddress] = useState(userData.address || '');
+
   const [phoneError, setPhoneError] = useState('');
   const [addressError, setAddressError] = useState('');
 
@@ -91,8 +93,7 @@ const Cart: React.FC = () => {
       (acc, item) => acc + item.productPrice * item.quantity,
       0
     );
-    const cartSum = subtotal + subtotal * (venueData.serviceFeePercent / 100);
-    return cartSum;
+    return subtotal + subtotal * (venueData.serviceFeePercent / 100);
   };
 
   const handleClose = () => {
@@ -110,8 +111,11 @@ const Cart: React.FC = () => {
 
   const handlePhoneChange = (value: string) => {
     setPhoneNumber(value);
-    if (value.length < 12) {
-      setPhoneError('Минимум 12 символов');
+
+    if (!value.trim()) {
+      setPhoneError('Это обязательное поле');
+    } else if (value.length < 13) {
+      setPhoneError('Тут нужно минимум 12 символов');
     } else {
       setPhoneError('');
     }
@@ -119,19 +123,29 @@ const Cart: React.FC = () => {
 
   const handleAddressChange = (value: string) => {
     setAddress(value);
-    if (value.trim().length < 4) {
-      setAddressError('Слишком короткий адрес');
+
+    if (!value.trim()) {
+      setAddressError('Это обязательное поле');
+    } else if (value.trim().length < 4) {
+      setAddressError('Тут нужно минимум 4 символа');
     } else {
       setAddressError('');
     }
   };
 
   const isButtonDisabled = useMemo(() => {
-    if (phoneError || addressError) {
+    if (phoneError) return false;
+
+    const isDelivery = orderTypes[activeIndex]?.value === 3;
+
+    if (isDelivery && addressError) return false;
+
+    if (!phoneNumber.trim() || phoneNumber.length < 12) return false;
+    if (isDelivery && (!address.trim() || address.trim().length < 4))
       return false;
-    }
-    return phoneNumber.length >= 13 && address.trim().length > 3;
-  }, [phoneNumber, address, phoneError, addressError]);
+
+    return true;
+  }, [phoneNumber, address, phoneError, addressError, activeIndex, orderTypes]);
 
   const handleOrder = async () => {
     setIsLoading(true);
@@ -204,11 +218,10 @@ const Cart: React.FC = () => {
 
   useEffect(() => {
     window.scrollTo(0, 0);
+
     if (userData.type) {
-      const idx = orderTypes.findIndex((item) => item.value === userData.type);
-      if (idx !== -1) {
-        setActiveIndex(idx);
-      }
+      const idx = orderTypes.findIndex((it) => it.value === userData.type);
+      if (idx >= 0) setActiveIndex(idx);
     }
   }, [userData.type, orderTypes]);
 
@@ -273,19 +286,19 @@ const Cart: React.FC = () => {
             <>
               {!venueData?.table?.tableNum && (
                 <div className='cart__order-type'>
-                  {orderTypes.map((item, index) => (
+                  {orderTypes.map((item, idx) => (
                     <div
                       key={item.value}
-                      onClick={() => handleClick(index)}
+                      onClick={() => handleClick(idx)}
                       className={`cart__order-type-wrapper bg-[#fff] border-[#e1e2e5] cursor-pointer ${
-                        activeIndex === index ? 'active' : ''
+                        activeIndex === idx ? 'active' : ''
                       }`}
                       style={{
                         borderColor:
-                          activeIndex === index ? colorTheme : '#e1e2e5',
+                          activeIndex === idx ? colorTheme : '#e1e2e5',
                       }}
                     >
-                      {activeIndex === index ? (
+                      {activeIndex === idx ? (
                         <svg
                           width='20'
                           height='20'
@@ -294,7 +307,7 @@ const Cart: React.FC = () => {
                         >
                           <g clipPath='url(#clip0_248_22508)'>
                             <path
-                              d='M10 1.875C8.39303 1.875 6.82214 2.35152 5.486 3.24431C4.14985 4.1371 3.10844 5.40605 2.49348 6.8907C1.87852 8.37535 1.71762 10.009 2.03112 11.5851C2.34463 13.1612 3.11846 14.6089 4.25476 15.7452C5.39106 16.8815 6.8388 17.6554 8.4149 17.9689C9.99099 18.2824 11.6247 18.1215 13.1093 17.5065C14.594 16.8916 15.8629 15.8502 16.7557 14.514C17.6485 13.1779 18.125 11.607 18.125 10C18.1227 7.84581 17.266 5.78051 15.7427 4.25727C14.2195 2.73403 12.1542 1.87727 10 1.875ZM13.5672 8.56719L9.19219 12.9422C9.13415 13.0003 9.06522 13.0464 8.98934 13.0779C8.91347 13.1093 8.83214 13.1255 8.75 13.1255C8.66787 13.1255 8.58654 13.1093 8.51067 13.0779C8.43415 13.0464 8.36542 13.0003 8.30757 12.9422L6.43257 11.0672C6.31552 10.9499 6.24983 10.7909 6.24983 10.625C6.24983 10.4591 6.31552 10.3002 6.43257 10.1828C6.54962 10.0653 6.70871 9.99963 6.87458 9.99963C7.04044 9.99963 7.19953 10.0653 7.31658 10.1828L8.75 11.6164L12.6828 7.68281C12.741 7.62474 12.8099 7.57868 12.8858 7.54725C12.9616 7.51583 13.043 7.49967 13.125 7.49967C13.207 7.49967 13.2884 7.51583 13.3642 7.54725C13.44 7.57868 13.5089 7.62474 13.5672 7.68281C13.6253 7.74088 13.6713 7.80982 13.7027 7.88569C13.7341 7.96157 13.7503 8.04292 13.7503 8.12499C13.7503 8.20706 13.7341 8.28841 13.7027 8.36428C13.6713 8.44016 13.6253 8.50909 13.5672 8.56719Z'
+                              d='M10 1.875C8.39303 1.875 6.82214 2.35152 5.486 3.24431C4.14985 4.1371 3.10844 5.40605 2.49348 6.8907C1.87852 8.37535 1.71762 10.009 2.03112 11.5851C2.34463 13.1612 3.11846 14.6089 4.25476 15.7452C5.39106 16.8815 6.8388 17.6554 8.4149 17.9689C9.99099 18.2824 11.6247 18.1215 13.1093 17.5065C14.594 16.8916 15.8629 15.8502 16.7557 14.514C17.6485 13.1779 18.125 11.607 18.125 10C18.1227 7.84581 17.266 5.78051 15.7427 4.25727C14.2195 2.73403 12.1542 1.87727 10 1.875ZM13.5672 8.56719L9.19219 12.9422C9.13415 13.0003 9.06522 13.0464 8.98934 13.0779C8.91347 13.1093 8.83214 13.1255 8.75 13.1255C8.66787 13.1255 8.58654 13.1093 8.51067 13.0779C8.43415 13.0464 8.36542 13.0003 8.30766 12.9422L6.43266 11.0672C6.31561 10.9499 6.24992 10.7909 6.24992 10.625C6.24992 10.4591 6.31561 10.3002 6.43266 10.1828C6.54971 10.0653 6.7088 9.99961 6.87466 9.99961C7.04053 9.99961 7.19962 10.0653 7.31667 10.1828L8.75 11.6164L12.6828 7.68281C12.741 7.62474 12.8099 7.57868 12.8858 7.54725C12.9616 7.51583 13.0429 7.49967 13.125 7.49967C13.2071 7.49967 13.2884 7.51583 13.3642 7.54725C13.44 7.57868 13.5089 7.62474 13.5672 7.68281C13.6253 7.74088 13.6713 7.80982 13.7027 7.88569C13.7341 7.96157 13.7503 8.04291 13.7503 8.12499C13.7503 8.20706 13.7341 8.2884 13.7027 8.36428C13.6713 8.44016 13.6253 8.50909 13.5672 8.56719Z'
                               fill={colorTheme}
                             />
                           </g>
@@ -434,7 +447,7 @@ const Cart: React.FC = () => {
             style={{ backgroundColor: colorTheme }}
             onClick={handleOrder}
           >
-            {t('button.next') || 'Далее'}
+            {t('orders.next') || 'Далее'}
           </button>
         </footer>
       )}
