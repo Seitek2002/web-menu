2bfd7baec8d25b2bf041d9f4b902340cea130598
Seitek2002 <seitekbegaliev@gmail.com>
2025-09-12 21:08:18 +0600

    test

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,9 @@
 import { FC, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
+import { useAppSelector } from 'hooks/useAppSelector';
+
 import arrowIcon from 'assets/icons/Header/arrow.svg';
-import logoIcon from 'assets/icons/Header/logo.svg';
 import search from 'assets/icons/Header/search.svg';
 
 import './style.scss';
@@ -28,6 +29,8 @@ const Header: FC<IProps> = ({ searchText, setSearchText }) => {
     [i18n.language]
   );
   const { t } = useTranslation();
+  const colorTheme =
+    useAppSelector((s) => s.yourFeature.venue?.colorTheme) || '#FD0151';
   const toggleLanguageMenu = () => setIsLanguageOpen((prev) => !prev);
 
   const selectLanguage = (language: string) => {
@@ -44,7 +47,20 @@ const Header: FC<IProps> = ({ searchText, setSearchText }) => {
     <header className='header'>
       <div className='header__content'>
         <div className='logo'>
-          <img src={logoIcon} alt='iMenu Logo' />
+          <svg
+            width='32'
+            height='32'
+            viewBox='0 0 32 32'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+            role='img'
+            aria-label='iMenu Logo'
+          >
+            <rect width='32' height='32' rx='16' fill={colorTheme} />
+            <path d='M21.4976 18.7638C21.4976 17.2393 20.9184 15.7773 19.8874 14.6993C18.8564 13.6214 17.458 13.0158 16 13.0158C14.5419 13.0158 13.1436 13.6214 12.1125 14.6993C11.0815 15.7773 10.5023 17.2393 10.5023 18.7638L21.4976 18.7638Z' fill='white' />
+            <path d='M9.49805 19.1133H22.5021V20.8635H9.49805V19.1133Z' fill='white' />
+            <path d='M16.8561 11.9399C16.8561 12.3836 16.4711 12.7433 15.9963 12.7433C15.5215 12.7433 15.1365 12.3836 15.1365 11.9399C15.1365 11.4962 15.5215 11.1365 15.9963 11.1365C16.4711 11.1365 16.8561 11.4962 16.8561 11.9399Z' fill='white' />
+          </svg>
           <span>iMenu.kg</span>
         </div>
 
